6a46f2b47fc55a87dad0ad189215951b1313e023
carlospq17 <carlospq_17@hotmail.com>
2021-12-29 10:14:05 -0600

    Identacion

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -18,39 +18,40 @@ import io.jsonwebtoken.SignatureAlgorithm;
 @RestController
 public class UserController {
 
-    @PostMapping("/user")
-    public User login(@RequestParam("user") String username, @RequestParam("password") String pwd) {
-        System.out.println("A");
-        String token = getJWTToken(username);
-        System.out.println("B");
-        User user = new User();
-        System.out.println("C");
-        user.setUser(username);
-        System.out.println("D");
-        user.setToken(token);
-        System.out.println("E" + user.toString() + user.getClass().getName());
-        return user;
-    }
+	@PostMapping("/user")
+	public User login(@RequestParam("user") String username, @RequestParam("password") String pwd) {
+		System.out.println("A");
+		String token = getJWTToken(username);
+		System.out.println("B");
+		User user = new User();
+		System.out.println("C");
+		user.setUser(username);
+		System.out.println("D");
+		user.setToken(token);
+		System.out.println("E" + user.toString() + user.getClass().getName());
+		User response = user;
+		return response;
+	}
 
-    private String getJWTToken(String username) {
-        String secretKey = "mySecretKey";
-        List<GrantedAuthority> grantedAuthorities = AuthorityUtils
-                .commaSeparatedStringToAuthorityList("ROLE_USER");
+	private String getJWTToken(String username) {
+		String secretKey = "mySecretKey";
+		List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+				.commaSeparatedStringToAuthorityList("ROLE_USER");
 
-        String token = Jwts
-                .builder()
-                .setId("softtekJWT")
-                .setSubject(username)
-                .claim("authorities",
-                        grantedAuthorities.stream()
-                                .map(GrantedAuthority::getAuthority)
-                                .collect(Collectors.toList()))
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 600000))
-                .signWith(SignatureAlgorithm.HS512,
-                        secretKey.getBytes())
-                .compact();
+		String token = Jwts
+				.builder()
+				.setId("softtekJWT")
+				.setSubject(username)
+				.claim("authorities",
+						grantedAuthorities.stream()
+								.map(GrantedAuthority::getAuthority)
+								.collect(Collectors.toList()))
+				.setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + 600000))
+				.signWith(SignatureAlgorithm.HS512,
+						secretKey.getBytes())
+				.compact();
 
-        return "Bearer " + token;
-    }
+		return "Bearer " + token;
+	}
 }
